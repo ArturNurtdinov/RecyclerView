d1233095a7f3ccc32b452e0b13e0bbf02f8d6b2e
Artur_Nurtdinov <a.nurtdnow@yandex.ru>
2019-07-31 23:07:35 +0300

    Small fixes

--- app/src/main/java/com/example/recycler/FinishActivity.kt
+++ app/src/main/java/com/example/recycler/FinishActivity.kt
@@ -1,5 +1,6 @@
 package com.example.recycler
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -7,13 +8,18 @@ import kotlinx.android.synthetic.main.activity_finish.*
 
 class FinishActivity : AppCompatActivity() {
 
-    companion object{
+    companion object {
         val KEY = "FINISH_ACTIVITY_KEY"
     }
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
 
-        textView.setText(intent.getStringExtra(KEY), TextView.BufferType.NORMAL)
+        textView.setText(
+            "Entry was made with element ${intent.getStringExtra(KEY)}",
+            TextView.BufferType.NORMAL
+        )
     }
 }
